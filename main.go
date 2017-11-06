842a6024baddb5e01b1f10af9c8e394847ee155b
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-11-06 11:29:11 -0500

    update main

--- main.go
+++ main.go
@@ -1,52 +1,42 @@
 package main
-  
-import (  
-  "os"
-  "fmt"
-  "log"
-  "bytes"
-  "strings"
-  "os/exec"
-  "io/ioutil"
-  "gopkg.in/yaml.v2"
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"os/exec"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
 )
 
 type Job struct {
-  Name  string `json:"name"`
-  Action  string `json:"action"`
+	Name 		string 		`json:"name"`
+	Command		string 		`json:"command"`
+	Arguments	[]string 	`json:arguments`
 }
 
 func main() {
-  job := Job{}
+	job := Job{}
 
-  filename, f_err := ioutil.ReadFile(string(os.Args[1]))
-  if f_err != nil {
-    log.Fatal("error: %v", f_err)
-  }
+	filename, f_err := ioutil.ReadFile(string(os.Args[1]))
+	if f_err != nil {
+		log.Fatal("error: %v", f_err)
+	}
 
-  err := yaml.Unmarshal(filename, &job)
-  if err != nil {
-    log.Fatal("error: %v", err)
-  }
-  fmt.Printf("job: \n%v\n\n", job)
+	err := yaml.Unmarshal(filename, &job)
+	if err != nil {
+		log.Fatal("error: %v", err)
+	}
+	fmt.Printf("job: \n%v\n\n", job)
 
-  //fmt.Printf("action: %v\n", job.Action)
-  cmd_elem := strings.Split(job.Action, " ")
-  var str bytes.Buffer
-  for _, i := range cmd_elem {
-    str.WriteString(i)
-    str.WriteString(" ")
-  }
-  fmt.Printf("action: %v\n", str.String())
+	cmd := job.Command
+	args := job.Arguments
+	var out []byte
 
+	if out, err = exec.Command(cmd, args...).Output(); err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
 
-  cmd := exec.Command(str.String())
-  var out bytes.Buffer
-  cmd.Stdout = &out
-  c_err := cmd.Run()
-  if err != nil {
-    log.Fatal(c_err)
-  }
-
-  fmt.Printf("%q\n", out.String())
+	fmt.Printf(string(out))
 }
